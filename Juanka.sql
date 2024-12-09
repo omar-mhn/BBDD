26800052ce71b970845310f56ff2491353fbd130
Juan Carlos <carlos85347@gmail.com>
2024-12-09 17:40:46 +0100

    base de dades

--- Juanka.sql
+++ Juanka.sql
@@ -4,10 +4,44 @@ CREATE TABLE COMPANYA (
     CODE3 VARCHAR (6) NOT NULL,
     ICAO VARCHAR (6) NOT NULL,
     pais VARCHAR (40) NOT NULL
-)
+);
 
 CREATE TABLE AVIO (
     num_serie VARCHAR (30) NOT NULL,
     tipus VARCHAR (10)  NOT NULL, 
     fabricant VARCHAR (20) NOT NULL,
-    any_fabricacio )
+    any_fabricacio YEAR 
+);
+
+CREATE TABLE AEROPORT (
+    codi VARCHAR (4) NOT NULL,
+    pais VARCHAR (40) NOT NULL,
+    ciutat VARCHAR (40) NOT NULL,
+    IATA  VARCHAR (4),
+    nom VARCHAR (55) NOT NULL,
+    any_construccio YEAR 
+);  
+
+CREATE TABLE MOSTRADOR (
+    numero TINYINT UNSIGNED
+);
+
+CREATE TABLE PERSONAL (
+    num_empleat TINYINT (8),
+    nom VARCHAR (25) NOT NULL,
+    cognom VARCHAR (35) NOT NULL,
+    passaport VARCHAR (20) NOT NULL,
+    sou float NOT NULL
+);
+
+CREATE TABLE PILOT (
+    hores_vol TIME
+);
+
+CREATE TABLE PASSATGER (
+    passaport VARCHAR (20) NOT NULL,
+    nom VARCHAR (30) NOT NULL,
+    cognom VARCHAR (50),
+    email VARCHAR (40),
+    data_naixement DATE  
+);
